c2ead0d64182c3ee0ac0ab5d758c544b7595259b
ChristianWillyFosso <113677026+ChristianWillyFosso@users.noreply.github.com>
2024-09-28 10:06:44 -0400

    Changer postgres pour neo4j

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/heartbeat', methods=['GET'])
 def heartbeat():
-    return jsonify({"villeChoisie": "Trois-Rivières"})
+    return jsonify({"villeChoisie": "Rimouski"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
